0c9b595c41eb30e1384dfa647bc14c457f42640e
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-08-19 16:05:39 +0300

    dowel do footera

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(function(){
-    $('.slider__box').slick();
+    $('.slider__box').slick({
+        prevArrow: '<img src="img/arrow-left.svg" alt="arrow-left" class="slider__arrow slider__arrow-left">',
+        nextArrow: '<img src="img/arrow-right.svg" alt="arrow-right" class="slider__arrow slider__arrow-right">',
+    });
 });
