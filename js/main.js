904b7109fa52d8d4c69128a0bba8869a78cc92c1
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-05 19:18:09 +0200

    add burger menu

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function(){
         prevArrow: '<img src="img/arrow-left.svg" alt="arrow-left" class="slider__arrow slider__arrow-left">',
         nextArrow: '<img src="img/arrow-right.svg" alt="arrow-right" class="slider__arrow slider__arrow-right">',
     });
+
+    $('.menu-btn').on('click', function (){
+        $('.menu__list').toggleClass('active');
+    });
 });
