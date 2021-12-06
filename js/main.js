85d8274db60eda0a5b7c3a3e71b6a220cbdcd957
Fabarm <87820977+Fabarm@users.noreply.github.com>
2021-12-07 00:24:54 +0200

    add adaptive from page 4

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(function(){
     $('.slider__box').slick({
         prevArrow: '<img src="img/arrow-left.svg" alt="arrow-left" class="slider__arrow slider__arrow-left">',
         nextArrow: '<img src="img/arrow-right.svg" alt="arrow-right" class="slider__arrow slider__arrow-right">',
+        responsive:[
+            {
+                breakpoint: 481,
+                settings:{
+                    arrows: false
+                }
+            }
+        ]
     });
 
     $('.menu-btn').on('click', function (){
